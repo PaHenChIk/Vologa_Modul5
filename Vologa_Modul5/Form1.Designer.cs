673fdefa26f6336ef0d13e5d7e6ef92cc1c2a010
PaHenChIk <pashavologa6@gmail.com>
2023-10-23 10:47:46 +0300

    м

---
+++ Vologa_Modul5/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Vologa_Modul5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.ravno = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.umnozh = new System.Windows.Forms.Button();
+            this.koren = new System.Windows.Forms.Button();
+            this.delenie = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(15, 119);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(53, 50);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(15, 175);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(53, 49);
+            this.b2.TabIndex = 1;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(15, 230);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(53, 50);
+            this.b3.TabIndex = 2;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(74, 119);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(53, 50);
+            this.b4.TabIndex = 3;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(74, 175);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(53, 49);
+            this.b5.TabIndex = 4;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(74, 230);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(53, 50);
+            this.b6.TabIndex = 5;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(133, 119);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(53, 50);
+            this.b7.TabIndex = 6;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(133, 175);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(53, 49);
+            this.b8.TabIndex = 7;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(133, 230);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(53, 50);
+            this.b9.TabIndex = 8;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(15, 287);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(92, 50);
+            this.b0.TabIndex = 9;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.b0_Click);
+            // 
+            // ravno
+            // 
+            this.ravno.Location = new System.Drawing.Point(113, 286);
+            this.ravno.Name = "ravno";
+            this.ravno.Size = new System.Drawing.Size(75, 51);
+            this.ravno.TabIndex = 10;
+            this.ravno.Text = "=";
+            this.ravno.UseVisualStyleBackColor = true;
+            this.ravno.Click += new System.EventHandler(this.ravno_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(15, 64);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(53, 50);
+            this.plus.TabIndex = 11;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(74, 64);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(53, 49);
+            this.minus.TabIndex = 12;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // umnozh
+            // 
+            this.umnozh.Location = new System.Drawing.Point(133, 64);
+            this.umnozh.Name = "umnozh";
+            this.umnozh.Size = new System.Drawing.Size(55, 49);
+            this.umnozh.TabIndex = 13;
+            this.umnozh.Text = "×";
+            this.umnozh.UseVisualStyleBackColor = true;
+            this.umnozh.Click += new System.EventHandler(this.umnozh_Click);
+            // 
+            // koren
+            // 
+            this.koren.Location = new System.Drawing.Point(192, 175);
+            this.koren.Name = "koren";
+            this.koren.Size = new System.Drawing.Size(53, 49);
+            this.koren.TabIndex = 14;
+            this.koren.Text = "√";
+            this.koren.UseVisualStyleBackColor = true;
+            this.koren.Click += new System.EventHandler(this.koren_Click);
+            // 
+            // delenie
+            // 
+            this.delenie.Location = new System.Drawing.Point(192, 119);
+            this.delenie.Name = "delenie";
+            this.delenie.Size = new System.Drawing.Size(53, 50);
+            this.delenie.TabIndex = 15;
+            this.delenie.Text = "/";
+            this.delenie.UseVisualStyleBackColor = true;
+            this.delenie.Click += new System.EventHandler(this.delenie_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 9);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(294, 49);
+            this.richTextBox1.TabIndex = 16;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 353);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.delenie);
+            this.Controls.Add(this.koren);
+            this.Controls.Add(this.umnozh);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.ravno);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(330, 392);
+            this.MinimumSize = new System.Drawing.Size(330, 392);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button ravno;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button umnozh;
+        private System.Windows.Forms.Button koren;
+        private System.Windows.Forms.Button delenie;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Vologa_Modul5/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vologa_Modul5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       private void b0_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "0";
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "1";
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "2";
+        }
+        private void b3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "3";
+        }
+        private void b4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "4";
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "5";
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "6";
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "7";
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "8";
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "9";
+        }
+
+        private void ravno_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var tokens = Tokenize(richTextBox1.Text);
+                var rpnExpression = ConvertToRPN(tokens);
+                var result = EvaluateRPN(rpnExpression);
+                richTextBox1.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text = "Ошибка: " + ex.Message;
+            }
+        }
+
+        private List<string> Tokenize(string expression)
+        {
+            var tokens = new List<string>();
+            var number = new StringBuilder();
+
+            foreach (var ch in expression)
+            {
+                if (char.IsDigit(ch) || ch == ','|| ch == '.')
+                {
+                    number.Append(ch);
+                }
+                else
+                {
+                    if (number.Length > 0)
+                    {
+                        tokens.Add(number.ToString());
+                        number.Clear();
+                    }
+
+                    if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '(' || ch == ')')
+                    {
+                        tokens.Add(ch.ToString());
+                    }
+                }
+            }
+
+            if (number.Length > 0)
+            {
+                tokens.Add(number.ToString());
+            }
+
+            return tokens;
+        }
+
+        private List<string> ConvertToRPN(List<string> tokens)
+        {
+            var output = new List<string>();
+            var operators = new Stack<string>();
+
+            foreach (var token in tokens)
+            {
+                if (double.TryParse(token, out _))
+                {
+                    output.Add(token);
+                }
+                else
+                {
+                    if (token == "(")
+                    {
+                        operators.Push(token);
+                    }
+                    else if (token == ")")
+                    {
+                        while (operators.Count > 0 && operators.Peek() != "(")
+                        {
+                            output.Add(operators.Pop());
+                        }
+                        operators.Pop(); 
+                    }
+                    else
+                    {
+                        while (operators.Count > 0 && GetPriority(operators.Peek()) >= GetPriority(token))
+                        {
+                            output.Add(operators.Pop());
+                        }
+                        operators.Push(token);
+                    }
+                }
+            }
+
+            while (operators.Count > 0)
+            {
+                output.Add(operators.Pop());
+            }
+
+            return output;
+        }
+
+
+        private int GetPriority(string op)
+        {
+            switch (op)
+            {
+                case "+":
+                case "-":
+                    return 1;
+                case "*":
+                case "/":
+                    return 2;
+                default:
+                    return 0;
+            }
+        }
+
+        private double EvaluateRPN(List<string> rpnExpression)
+        {
+            var numbers = new Stack<double>();
+
+            foreach (var token in rpnExpression)
+            {
+                if (double.TryParse(token, out var number))
+                {
+                    numbers.Push(number);
+                }
+                else
+                {
+                    var rightOperand = numbers.Pop();
+                    var leftOperand = numbers.Pop();
+
+                    switch (token)
+                    {
+                        case "+":
+                            numbers.Push(leftOperand + rightOperand);
+                            break;
+                        case "-":
+                            numbers.Push(leftOperand - rightOperand);
+                            break;
+                        case "*":
+                            numbers.Push(leftOperand * rightOperand);
+                            break;
+                        case "/":
+                            numbers.Push(leftOperand / rightOperand);
+                            break;
+                    }
+                }
+            }
+
+            return numbers.Pop();
+        }
+
+
+
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "+";
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "-";
+        }
+
+        private void umnozh_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "*";
+        }
+
+        private void delenie_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "/";
+        }
+
+        private void koren_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal number = Decimal.Parse(richTextBox1.Text);
+                var result = Math.Sqrt((double)number);
+                richTextBox1.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text = "Ошибка: " + ex.Message;
+            }
+
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
